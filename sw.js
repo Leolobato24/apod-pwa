20fd42e167187f839ef56e93780974ccf30a3c7e
Leolobato24 <cazareslobatol@gmail.com>
2025-11-20 13:37:38 -0600

    Update sw.js
    Signed-off-by: Leolobato24 <cazareslobatol@gmail.com>

--- sw.js
+++ sw.js
@@ -1,6 +1,26 @@
-self.addEventListener("install", e=>{
-  self.skipWaiting();
+const CACHE_NAME = "apod-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./nasa.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
-self.addEventListener("fetch", e=>{
-  e.respondWith(fetch(e.request));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
+
