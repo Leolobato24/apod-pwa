e553ac579f904cb571164df1557e0be2ee08a21b
Leolobato24 <cazareslobatol@gmail.com>
2025-11-20 13:04:03 -0600

    Add files via upload
    Signed-off-by: Leolobato24 <cazareslobatol@gmail.com>

---
+++ nasa.js
@@ -0,0 +1,13 @@
+const API_KEY = "DEMO_KEY"; 
+const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    const c = document.getElementById("content");
+    if(data.media_type === "image"){
+       c.innerHTML = `<h2>${data.title}</h2><img src="${data.url}" width="80%"> <p>${data.explanation}</p>`;
+    } else {
+       c.innerHTML = `<h2>${data.title}</h2><iframe width="560" height="315" src="${data.url}" frameborder="0" allowfullscreen></iframe><p>${data.explanation}</p>`;
+    }
+  });
